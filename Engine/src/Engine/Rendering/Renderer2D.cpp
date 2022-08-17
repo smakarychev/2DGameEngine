b4fa26d6da942463bc5f27a282e7b6052cfb063c
Сергей Макарычев <sm05112001@gmail.com>
2022-08-18 01:09:20 +0700

    Big changes in Renderer2D
    added textures, rotation, polygons, multiple batches.
    all transform made "inline" since in was quite a bit faster, than using glm::mat4.

--- Engine/src/Engine/Rendering/Renderer2D.cpp
+++ Engine/src/Engine/Rendering/Renderer2D.cpp
@@ -15,47 +15,61 @@ namespace Engine
 		// Initialize batch shader and batch buffers.
 		s_BatchData.BatchShader = Shader::ReadShaderFromFile("assets/shaders/batchShader.glsl");
 		
+		//*************** Init quad **********************************************************
+		s_BatchData.ReferenceQuad.Position.resize(4);
 		s_BatchData.ReferenceQuad.Position[0] = glm::vec4(-0.5f, -0.5f, 0.0f, 1.0f);
 		s_BatchData.ReferenceQuad.Position[1] = glm::vec4( 0.5f, -0.5f, 0.0f, 1.0f);
 		s_BatchData.ReferenceQuad.Position[2] = glm::vec4( 0.5f,  0.5f, 0.0f, 1.0f);
 		s_BatchData.ReferenceQuad.Position[3] = glm::vec4(-0.5f,  0.5f, 0.0f, 1.0f);
 		
+		s_BatchData.ReferenceQuad.UV.resize(4);
 		s_BatchData.ReferenceQuad.UV[0] = glm::vec2(0.0f, 0.0f);
 		s_BatchData.ReferenceQuad.UV[1] = glm::vec2(1.0f, 0.0f);
 		s_BatchData.ReferenceQuad.UV[2] = glm::vec2(1.0f, 1.0f);
 		s_BatchData.ReferenceQuad.UV[3] = glm::vec2(0.0f, 1.0f);
 		
-		s_BatchData.ReferenceQuad.Color[0] = glm::vec4(1.0f);
-		s_BatchData.ReferenceQuad.Color[1] = glm::vec4(1.0f);
-		s_BatchData.ReferenceQuad.Color[2] = glm::vec4(1.0f);
-		s_BatchData.ReferenceQuad.Color[3] = glm::vec4(1.0f);
-
 		// Indices structure is predictable.
-		
-		U32* indices = NewArr<U32>(s_BatchData.MaxVertices);
-		for (U32 i = 0; i < s_BatchData.MaxVertices; i += 6)
+		BatchData quadBatch;
+		U32* indices = NewArr<U32>(quadBatch.MaxIndices);
+		for (U32 i = 0; i + 6 < quadBatch.MaxIndices; i += 6)
 		{
-			U32 currVer = s_BatchData.CurrentVertices;
-			indices[i] = currVer + 0;
-			indices[i + 1] = currVer + 1;
-			indices[i + 2] = currVer + 3;
-			indices[i + 3] = currVer + 1;
-			indices[i + 4] = currVer + 3;
-			indices[i + 5] = currVer + 2;
-			s_BatchData.CurrentVertices += 4;
+			U32 currVer = quadBatch.CurrentVertices;
+			indices[i]		= currVer + 0;
+			indices[i + 1]	= currVer + 1;
+			indices[i + 2]	= currVer + 3;
+			indices[i + 3]	= currVer + 1;
+			indices[i + 4]	= currVer + 3;
+			indices[i + 5]	= currVer + 2;
+			quadBatch.CurrentVertices += 4;
 		}
-		
-		auto vbo = VertexBuffer::Create(nullptr, s_BatchData.MaxVertices * sizeof(Vertex1P1UV1C));
-		vbo->SetVertexLayout(Vertex1P1UV1C::GetLayout());
-		auto ibo = IndexBuffer::Create(indices, s_BatchData.MaxVertices);
-		DeleteArr<U32>(indices, s_BatchData.MaxVertices);
+		quadBatch.CurrentVertices = 0;
 
-		s_BatchData.BatchVAO = VertexArray::Create();
-		s_BatchData.BatchVAO->AddVertexBuffer(vbo);
-		s_BatchData.BatchVAO->SetIndexBuffer(ibo);
+		auto vbo = VertexBuffer::Create(nullptr, quadBatch.MaxVertices * sizeof(BatchVertex));
+		vbo->SetVertexLayout(BatchVertex::GetLayout());
+		auto ibo = IndexBuffer::Create(indices, quadBatch.MaxIndices);
+		DeleteArr<U32>(indices, quadBatch.MaxIndices);
+		quadBatch.VAO = VertexArray::Create();
+		quadBatch.VAO->AddVertexBuffer(vbo);
+		quadBatch.VAO->SetIndexBuffer(ibo);
 
-		s_BatchData.QuadsVerticesMemory = reinterpret_cast<U8*>(MemoryManager::Alloc(s_BatchData.MaxVertices * sizeof(Vertex1P1UV1C)));
-		s_BatchData.CurrentQuadVertexPointer = reinterpret_cast<Vertex1P1UV1C*>(s_BatchData.QuadsVerticesMemory);
+		quadBatch.VerticesMemory = NewArr<U8>(quadBatch.MaxVertices * sizeof(BatchVertex));
+		quadBatch.CurrentVertexPointer = reinterpret_cast<BatchVertex*>(quadBatch.VerticesMemory);
+		s_BatchData.QuadBatch = quadBatch;
+
+		//*************** Init regular polygon ***********************************************
+		BatchData polygonBatch;
+		vbo = VertexBuffer::Create(nullptr, polygonBatch.MaxVertices * sizeof(BatchVertex));
+		vbo->SetVertexLayout(BatchVertex::GetLayout());
+		ibo = IndexBuffer::Create(indices, polygonBatch.MaxIndices);
+		polygonBatch.VAO = VertexArray::Create();
+		polygonBatch.VAO->AddVertexBuffer(vbo);
+		polygonBatch.VAO->SetIndexBuffer(ibo);
+
+		polygonBatch.VerticesMemory = NewArr<U8>(polygonBatch.MaxVertices * sizeof(BatchVertex));
+		polygonBatch.CurrentVertexPointer = reinterpret_cast<BatchVertex*>(polygonBatch.VerticesMemory);
+		polygonBatch.IndicesMemory = NewArr<U8>(polygonBatch.MaxIndices * sizeof(U32));
+		polygonBatch.CurrentIndexPointer = reinterpret_cast<U32*>(polygonBatch.IndicesMemory);
+		s_BatchData.PolygonBatch = polygonBatch;
 	}
 
 	void Renderer2D::BeginScene(std::shared_ptr<Camera> camera)
@@ -65,65 +79,228 @@ namespace Engine
 	
 	void Renderer2D::EndScene()
 	{
-		Flush();
-		ResetBatch();
+		Flush(s_BatchData.QuadBatch);
+		ResetBatch(s_BatchData.QuadBatch);
+		Flush(s_BatchData.PolygonBatch);
+		ResetBatch(s_BatchData.PolygonBatch);
 		//ENGINE_INFO("Renderer2D total draw calls: {}", s_BatchData.DrawCalls);
 		s_BatchData.DrawCalls = 0;
-
 	}
 
-	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& dimensions, const glm::vec4& color)
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& scale, const glm::vec4& color)
 	{
-		if (s_BatchData.CurrentVertices + 4 > s_BatchData.MaxVertices)
-		{
-			Flush();
-			ResetBatch();
-		}
+		DrawQuad(position, scale, nullptr, s_BatchData.ReferenceQuad.UV, color);
+	}
 
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& scale, F32 rotation, const glm::vec4& color)
+	{
+		DrawQuad(position, scale, rotation, nullptr, {}, color);
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& scale, Texture& texture, const glm::vec2& textureTiling)
+	{
+		DrawQuad(position, scale, &texture, s_BatchData.ReferenceQuad.UV, glm::vec4(1.0f), textureTiling);
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& scale, F32 rotation, Texture& texture, const glm::vec2& textureTiling)
+	{
+		DrawQuad(position, scale, rotation, &texture, {}, glm::vec4(1.0f), textureTiling);
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& scale, Texture& texture, const glm::vec4& tint, const glm::vec2& textureTiling)
+	{
+		DrawQuad(position, scale, &texture, s_BatchData.ReferenceQuad.UV, tint, textureTiling);
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& scale, F32 rotation, Texture& texture, const glm::vec4& tint, const glm::vec2& textureTiling)
+	{
+		DrawQuad(position, scale, rotation, &texture, {}, tint, textureTiling);
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& scale, Texture& texture, const std::vector<glm::vec2>& uv, const glm::vec4& tint, const glm::vec2& textureTiling)
+	{
+		DrawQuad(position, scale, &texture, uv, tint, textureTiling);
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& scale, Texture* texture, const std::vector<glm::vec2>& uv, const glm::vec4& tint, const glm::vec2& textureTiling)
+	{
+		DrawQuad(position, scale, 0.0f, texture, uv, tint, textureTiling);
+	}
+
+	void Renderer2D::DrawQuad(
+		const glm::vec3& position, const glm::vec2& scale, F32 rotation,
+		Texture* texture, const std::vector<glm::vec2>& uv,
+		const glm::vec4& tint, const glm::vec2& textureTiling
+	)
+	{
+		BatchData& quadBatch = s_BatchData.QuadBatch;
+		if (quadBatch.CurrentVertices + 4 > quadBatch.MaxVertices || quadBatch.CurrentIndices + 6 > quadBatch.MaxIndices)
+		{
+			Flush(quadBatch);
+			ResetBatch(quadBatch);
+		}
+		F32 textureIndex = GetTextureIndex(quadBatch, texture);
 		auto& referenceQuad = s_BatchData.ReferenceQuad;
 
 		// Create new quad.
-		Vertex1P1UV1C vertices[4];
-		for (int i = 0; i < 4; i++)
+		for (U32 i = 0; i < 4; i++)
 		{
-			vertices[i].Position = glm::vec3(
-				referenceQuad.Position[i].x * dimensions.x + position.x,
-				referenceQuad.Position[i].y * dimensions.y + position.y,
-				position.z
-			);
-			vertices[i].UV = referenceQuad.UV[i];
-			vertices[i].Color = color;
+			BatchVertex* vertex = quadBatch.CurrentVertexPointer;
+			vertex->Position = glm::vec3(referenceQuad.Position[i]);
+			if (rotation == 0.0f) InitVertexGeometryData(*vertex, position, scale);
+			else InitVertexGeometryData(*vertex, position, scale, rotation);
+			InitVertexColorData(*vertex, textureIndex, uv[i], tint, textureTiling);
 
-			// Augment internal array.
-			*s_BatchData.CurrentQuadVertexPointer = vertices[i];
-			s_BatchData.CurrentQuadVertexPointer++;
+			quadBatch.CurrentVertexPointer++;
+		}
+		quadBatch.CurrentVertices += 4;
+		quadBatch.CurrentIndices += 6;
+	}
+
+	void Renderer2D::DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, const glm::vec4& color)
+	{
+		DrawPolygon(polygon, position, scale, nullptr, color);
+	}
+
+	void Renderer2D::DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, F32 rotation, const glm::vec4& color)
+	{
+		DrawPolygon(polygon, position, scale, rotation, nullptr, color);
+	}
+
+	void Renderer2D::DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, Texture& texture, const glm::vec2& textureTiling)
+	{
+		DrawPolygon(polygon, position, scale, &texture, glm::vec4(1.0f), textureTiling);
+	}
+
+	void Renderer2D::DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, F32 rotation, Texture& texture, const glm::vec2& textureTiling)
+	{
+		DrawPolygon(polygon, position, scale, rotation, &texture, glm::vec4(1.0f), textureTiling);
+	}
+
+	void Renderer2D::DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, Texture& texture, const glm::vec4& tint, const glm::vec2& textureTiling)
+	{
+		DrawPolygon(polygon, position, scale, &texture, tint, textureTiling);
+	}
+
+	void Renderer2D::DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, Texture* texture, const glm::vec4& tint, const glm::vec2& textureTiling)
+	{
+		DrawPolygon(polygon, position, scale, 0.0f, texture, tint, textureTiling);
+	}
+
+	void Renderer2D::DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, F32 rotation, Texture* texture, const glm::vec4& tint, const glm::vec2& textureTiling)
+	{
+		BatchData& polygonBatch = s_BatchData.PolygonBatch;
+		if (polygonBatch.CurrentVertices + polygon.GetVertices().size() > polygonBatch.MaxVertices)
+		{
+			Flush(s_BatchData.PolygonBatch);
+			ResetBatch(s_BatchData.PolygonBatch);
+		}
+		F32 textureIndex = GetTextureIndex(polygonBatch, texture);
+
+		for (U32 i = 0; i < polygon.GetNumberOfVertices(); i++)
+		{
+			BatchVertex* vertex = polygonBatch.CurrentVertexPointer;
+			vertex->Position = glm::vec3(polygon.GetVertices()[i], 0.0f);
+			if (rotation == 0.0f) InitVertexGeometryData(*vertex, position, scale);
+			else InitVertexGeometryData(*vertex, position, scale, rotation);
+			InitVertexColorData(*vertex, textureIndex, polygon.GetUVs()[i], tint, textureTiling);
+			polygonBatch.CurrentVertexPointer++;
+		}
+		for (U32 i = 0; i < polygon.GetIndices().size(); i++)
+		{
+			U32* index = polygonBatch.CurrentIndexPointer;
+			*index = polygon.GetIndices()[i] + polygonBatch.CurrentVertices;
+			polygonBatch.CurrentIndexPointer++;
+		}
+		polygonBatch.CurrentVertices += U32(polygon.GetVertices().size());
+		polygonBatch.CurrentIndices += U32(polygon.GetIndices().size());
+	}
+
+	void Renderer2D::Flush(BatchData& batch)
+	{
+		s_BatchData.BatchShader->Bind();
+
+		for (U32 i = 0; i < batch.CurrentTextureIndex; i++)
+		{
+			batch.UsedTextures[i]->Bind(i);
 		}
 
-		// Add quad to buffer.
-		ENGINE_CORE_ASSERT(s_BatchData.BatchVAO->GetVertexBuffers().size() == 1, "Batch must have only one vbo.");
-		s_BatchData.CurrentQuads++;
-		s_BatchData.CurrentVertices += 6;
-	}
+		ENGINE_CORE_ASSERT(batch.VAO->GetVertexBuffers().size() == 1, "Batch must have only one vbo.");
+		
+		batch.VAO->GetVertexBuffers()[0]->SetData(batch.VerticesMemory, sizeof(BatchVertex) * batch.CurrentVertices);
+		if (&batch == &s_BatchData.PolygonBatch) 
+			batch.VAO->GetIndexBuffer()->SetData(reinterpret_cast<U32*>(batch.IndicesMemory), batch.CurrentIndices);
 
-	void Renderer2D::Flush()
-	{
-		s_BatchData.BatchVAO->GetVertexBuffers()[0]->SetData(s_BatchData.QuadsVerticesMemory, sizeof(Vertex1P1UV1C) * s_BatchData.CurrentVertices);
-		s_BatchData.BatchShader->Bind();
 		s_BatchData.BatchShader->SetUniformMat4("u_modelViewProjection", s_BatchData.CameraViewProjection);
 		s_BatchData.DrawCalls++;
-		RenderCommand::DrawIndexed(s_BatchData.BatchVAO, s_BatchData.CurrentVertices);
+		RenderCommand::DrawIndexed(batch.VAO, batch.CurrentIndices);
 	}
 
-	void Renderer2D::ResetBatch()
+	void Renderer2D::ResetBatch(BatchData& batch)
 	{
-		s_BatchData.CurrentQuads = 0;
-		s_BatchData.CurrentVertices = 0;
-		s_BatchData.CurrentQuadVertexPointer = reinterpret_cast<Vertex1P1UV1C*>(s_BatchData.QuadsVerticesMemory);
+		batch.CurrentVertices = 0;
+		batch.CurrentIndices = 0;
+		batch.CurrentTextureIndex = 0;
+		batch.CurrentVertexPointer = reinterpret_cast<BatchVertex*>(batch.VerticesMemory);
+		batch.CurrentIndexPointer = reinterpret_cast<U32*>(batch.IndicesMemory);
 	}
+
+	void Renderer2D::InitVertexGeometryData(BatchVertex& vertex, const glm::vec3& position, const glm::vec2& scale)
+	{
+		vertex.Position = vertex.Position * glm::vec3(scale, 1.0) + position;
+	}
+
+	void Renderer2D::InitVertexGeometryData(BatchVertex& vertex, const glm::vec3& position, const glm::vec2& scale, F32 rotation)
+	{
+		vertex.Position = glm::vec3{
+			glm::cos(rotation) * vertex.Position.x - glm::sin(rotation) * vertex.Position.y,
+			glm::sin(rotation) * vertex.Position.x + glm::cos(rotation) * vertex.Position.y,
+			vertex.Position.z
+		};
+		vertex.Position = vertex.Position * glm::vec3(scale, 1.0) + position;
+	}
+
+	void Renderer2D::InitVertexColorData(BatchVertex& vertex, F32 textureIndex, const glm::vec2& uv, const glm::vec4& tint, const glm::vec2& textureTiling)
+	{
+		vertex.TextureIndex = textureIndex;
+		vertex.Color = tint;
+		vertex.TextureTiling = textureTiling;
+		vertex.UV = uv;
+	}
+
+	F32 Renderer2D::GetTextureIndex(BatchData& batch, Texture* texture)
+	{
+		F32 textureIndex = -1.0f;
+		if (texture != nullptr)
+		{
+			for (U32 i = 0; i < batch.CurrentTextureIndex; i++)
+			{
+				if (batch.UsedTextures[i] == texture) textureIndex = F32(i);
+			}
+			// If no such texture in the batch.
+			if (textureIndex == -1.0f)
+			{
+				if (batch.CurrentTextureIndex >= batch.MaxTextures)
+				{
+					Flush(batch);
+					ResetBatch(batch);
+				}
+				batch.UsedTextures[batch.CurrentTextureIndex] = const_cast<Texture*>(texture);
+				textureIndex = F32(batch.CurrentTextureIndex);
+				batch.CurrentTextureIndex++;
+			}
+		}
+		return textureIndex;
+	}
+
 
 	void Renderer2D::ShutDown()
 	{
 		s_BatchData.BatchShader.~shared_ptr();
-		s_BatchData.BatchVAO.~shared_ptr();
+		s_BatchData.QuadBatch.VAO.~shared_ptr();
+		s_BatchData.PolygonBatch.VAO.~shared_ptr();
+		DeleteArr<U8>(s_BatchData.QuadBatch.VerticesMemory, s_BatchData.QuadBatch.MaxVertices * sizeof(BatchVertex));
+		DeleteArr<U8>(s_BatchData.PolygonBatch.VerticesMemory, s_BatchData.PolygonBatch.MaxVertices * sizeof(BatchVertex));
+		DeleteArr<U8>(s_BatchData.PolygonBatch.IndicesMemory, s_BatchData.PolygonBatch.MaxIndices * sizeof(U32));
 	}
 }
--- Engine/src/Engine/Rendering/Renderer2D.h
+++ Engine/src/Engine/Rendering/Renderer2D.h
@@ -4,8 +4,10 @@
 
 #include "Engine/Rendering/Buffer.h"
 #include "Engine/Rendering/Shader.h"
+#include "Engine/Rendering/Texture.h"	
 
 #include "Engine/Core/Camera.h"
+#include "Engine/Primitives/2D/RegularPolygon.h"
 
 #include <glm/glm.hpp>
 
@@ -15,56 +17,73 @@ namespace Engine
 	class Renderer2D
 	{
 	public:
-		// TODO: reasonable name? (This is from Game engine architecture book.
-		struct Vertex1P1UV1C
+		struct BatchVertex
 		{
 			// vec3 for position to have the ability to render something on top.
-			glm::vec3 Position;
-			glm::vec2 UV;
-			glm::vec4 Color;
+			glm::vec3 Position		= glm::vec3(0.0f);
+			F32 TextureIndex		= -1.0f;
+			glm::vec2 UV			= glm::vec2(0.0f);
+			glm::vec2 TextureTiling	= glm::vec2(1.0f);
+			glm::vec4 Color			= glm::vec4(1.0f);
 
 			// Maybe just static member?
 			static const VertexLayout& GetLayout()
 			{
 				const static VertexLayout layout{ {
-					{ LayoutElement::Float3, "a_position" },
-					{ LayoutElement::Float2, "a_uv" },
-					{ LayoutElement::Float4, "a_color" },
+					{ LayoutElement::Float3,	"a_position" },
+					{ LayoutElement::Float,		"a_textureIndex" },
+					{ LayoutElement::Float2,	"a_uv" },
+					{ LayoutElement::Float2,	"a_textureTiling" },
+					{ LayoutElement::Float4,	"a_color" },
 				} };
 				return layout;
 			}
 
-			Vertex1P1UV1C(const glm::vec3& pos, const glm::vec2& uv, const glm::vec4 color) : 
+			BatchVertex(const glm::vec3& pos, const glm::vec2& uv, const glm::vec4 color) :
 				Position(pos), UV(uv), Color(color)
 			{}
-			Vertex1P1UV1C() = default;
+			BatchVertex(const glm::vec3& pos, const glm::vec2& uv, Texture& texture, F32 textureTiling = 1.0f) :
+				Position(pos), UV(uv), TextureIndex(F32(texture.GetId())), TextureTiling(textureTiling)
+			{}
+			BatchVertex() = default;
+		};
+
+		struct BatchData
+		{
+			// TODO: move to config.
+			U32 MaxVertices = U32(2_MiB) / sizeof(BatchVertex);
+			U32 MaxIndices = U32(2_MiB) / sizeof(U32);
+			U32 MaxTextures = 32;
+			Texture* UsedTextures[32] = { 0 };
+			U32 CurrentTextureIndex = 0;
+			U32 CurrentVertices = 0;
+			U32 CurrentIndices = 0;
+			std::shared_ptr<VertexArray> VAO;
+
+			U8* VerticesMemory = nullptr;
+			BatchVertex* CurrentVertexPointer = nullptr;
+			U8* IndicesMemory = nullptr;
+			U32* CurrentIndexPointer;
 		};
 
 		struct BatchRendererData
 		{
-			// TODO: move to config.
-			U32 MaxVertices = 2_MiB / sizeof(Vertex1P1UV1C);
-			U32 CurrentVertices = 0;
-			U32 CurrentQuads = 0;
-
 			U32 DrawCalls = 0;
 
 			std::shared_ptr<Shader> BatchShader;
-			std::shared_ptr<VertexArray> BatchVAO;
+			glm::mat4 CameraViewProjection = glm::mat4(1.0f);
 
 			struct ReferenceQuad
 			{
 				// Vec4 for alignment.
-				glm::vec4 Position[4];
-				glm::vec4 Color[4];
-				glm::vec2 UV[4];
+				std::vector<glm::vec4> Position;
+				std::vector<glm::vec2> UV;
 			};
 
-			U8* QuadsVerticesMemory = nullptr;
-			Vertex1P1UV1C* CurrentQuadVertexPointer = nullptr;
-
 			ReferenceQuad ReferenceQuad;
-			glm::mat4 CameraViewProjection = glm::mat4(1.0f);
+
+			BatchData QuadBatch;
+			BatchData PolygonBatch;
 		};
 
 	public:
@@ -74,10 +93,39 @@ namespace Engine
 		static void BeginScene(std::shared_ptr<Camera> camera);
 		static void EndScene();
 
-		static void DrawQuad(const glm::vec3& position, const glm::vec2& dimensions, const glm::vec4& color);
-		static void Flush();
-		static void ResetBatch();
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& scale, const glm::vec4& color);
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& scale, F32 rotation, const glm::vec4& color);
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& scale, Texture& texture, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& scale, F32 rotation, Texture& texture, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& scale, Texture& texture, const glm::vec4& tint, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& scale, F32 rotation, Texture& texture, const glm::vec4& tint, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& scale, Texture& texture, const std::vector<glm::vec2>& uv, const glm::vec4& tint, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& scale, Texture* texture, const std::vector<glm::vec2>& uv, const glm::vec4& tint, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static void DrawQuad(
+			const glm::vec3& position, const glm::vec2& scale, F32 rotation,
+			Texture* texture, const std::vector<glm::vec2>& uv, 
+			const glm::vec4& tint, const glm::vec2& textureTiling = glm::vec2(1.0f)
+		);
 
+		static void DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, const glm::vec4& color);
+		static void DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, F32 rotation, const glm::vec4& color);
+		static void DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, Texture& texture, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static void DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, F32 rotation, Texture& texture, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static void DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, Texture& texture, const glm::vec4& tint, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static void DrawPolygon(const RegularPolygon& polygon, const glm::vec3& position, const glm::vec2& scale, Texture* texture, const glm::vec4& tint, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static void DrawPolygon(
+			const RegularPolygon& polygon,
+			const glm::vec3& position, const glm::vec2& scale, F32 rotation,
+			Texture* texture, const glm::vec4& tint, const glm::vec2& textureTiling = glm::vec2(1.0f)
+		);
+
+		static void Flush(BatchData& batch);
+		static void ResetBatch(BatchData& batch);
+	private:
+		static void InitVertexGeometryData(BatchVertex& vertex, const glm::vec3& position, const glm::vec2& scale);
+		static void InitVertexGeometryData(BatchVertex& vertex, const glm::vec3& position, const glm::vec2& scale, F32 rotation);
+		static void InitVertexColorData(BatchVertex& vertex, F32 textureIndex, const glm::vec2& uv, const glm::vec4& tint, const glm::vec2& textureTiling = glm::vec2(1.0f));
+		static F32 GetTextureIndex(BatchData& batch, Texture* texture);
 	private:
 		static BatchRendererData s_BatchData;
 	};
